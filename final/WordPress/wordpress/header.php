27c5d7d98ca071cf727bd8e7bb3e58862b0d7af8
june0039 <74568248+june0039@users.noreply.github.com>
2022-04-20 21:07:27 -0400

    wrp

---
+++ final/WordPress/wordpress/header.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WordPress
+ * @subpackage MTM6303-Final
+ * @since MTM6303-Final 1.0
+ */
+
+
+function  MTM6303Final_setup()
+{
+}
+
+add_action('after_setup_theme', 'MTM6303Final_setup');
